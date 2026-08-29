09c146e71b7d145a80351e52e961f890f775f2c3
Oscar86tw <127740420+Oscar86tw@users.noreply.github.com>
2026-08-30 02:20:35 +0800

    UTOP-V5.1.3.40_快捷鍵展示雲端快速索引與號誌控制修正

--- config/app-config.js
+++ config/app-config.js
@@ -1,8 +1,8 @@
 export const APP_CONFIG = Object.freeze({
   appName: '昱拓弱電有限公司 UTOP-3D',
-  version: '5.1.3.39',
+  version: '5.1.3.40',
   defaultQuality: 'ultra',
-  revision: 'V51339_SHORTCUT_VISUAL_FEEDBACK',
+  revision: 'V51340_SHORTCUT_SHOWCASE_FAST_CLOUD',
   baseUiVersion: '1.1.4 Professional UI',
   spreadsheetId: '1lmEqbYals_uBIOrQFDupYR2hs0iks5zEJM6FTOuOwP0',
   configSheetName: '工作表1',
